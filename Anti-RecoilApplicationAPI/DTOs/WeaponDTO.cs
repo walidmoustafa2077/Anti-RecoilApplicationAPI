dd6369c6657a4e9adfca308d0b9aecfcac29254c
Walid Mostafa <153025777+walidmoustafa2077@users.noreply.github.com>
2024-12-21 04:22:48 +0200

    Revert "feat: Initialize ASP.NET Core project with core features"

--- Anti-RecoilApplicationAPI/DTOs/WeaponDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Anti_RecoilApplicationAPI.DTOs
-{
-    public class WeaponDTO
-    {
-        public string WeaponName { get; set; } = string.Empty;
-        public float Sensitivity { get; set; }
-        public string Pattern { get; set; } = string.Empty;
-    }
-}
